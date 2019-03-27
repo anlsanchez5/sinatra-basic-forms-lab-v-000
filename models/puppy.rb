effa62abf3c92a74ec7c30e3aa62a01b6eb8f571
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-27 18:11:30 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,4 +2,10 @@ class Puppy
   attr_reader :name, :breed
   attr_accessor :months_old
 
+  def initialize (name,breed, months_old)
+    @name = name
+    @breed = breed 
+    @months_old = months_old
+  end
+
 end
