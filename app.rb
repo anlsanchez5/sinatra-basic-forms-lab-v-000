5ab8f2713a857109778f8569e9f4586c8911b0c5
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-27 18:23:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/new' do
+  post '/puppy' do
     puppy = Puppy.new(params[:name], params[:breed], params[:age])
     @name = puppy.name
     @breed = puppy.age
