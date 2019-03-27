db8d289445b8b706ef18b9d175c79330b3872a81
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-27 18:28:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   post '/puppy' do
     puppy = Puppy.new(params[:name], params[:breed], params[:age])
     @name = puppy.name
-    @breed = puppy.age
+    @breed = puppy.breed
     @age = puppy.months_old
     erb :display_puppy
   end
